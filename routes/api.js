d5ac0b755dff5d1b00a0f5caae41542d86da9547
Sithembiso Maphanga <maphangasthembiso@gmail.com>
2022-08-22 09:22:05 +0200

    Creating the Routes

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,12 @@
+// Require the express framework mto create route modules that can be used by the API
 const express = require('express');
+// Create an instance of the router module of the express class
 const router = express.Router();
+// Require the the model with the schema object of the data to query
 const Todo = require('../models/todo');
 
+
+// view all tasks
 router.get('/todos', (req, res, next) => {
   // get placeholder
     // This will return all the data, exposing only the id and action field to the client
@@ -10,7 +15,7 @@ router.get('/todos', (req, res, next) => {
     .catch(next);
 });
 
-
+// Add/Create a task
 router.post('/todos', (req, res, next) => {
   // post placeholder
   if (req.body.action) {
@@ -24,7 +29,7 @@ router.post('/todos', (req, res, next) => {
   }
 });
 
-
+// delete a completed task
 router.delete('/todos/:id', (req, res, next) => {
   // delete placeholder
   Todo.findOneAndDelete({ _id: req.params.id })
@@ -33,5 +38,5 @@ router.delete('/todos/:id', (req, res, next) => {
 });
 
 
-
+// Export the router model so it can be imported by other modules
 module.exports = router;
